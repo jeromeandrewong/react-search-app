9418804d69f8fed76baa26fa7ebb61a60c862fe4
Jerome Wong <jeromewongjh@gmail.com>
2022-11-29 18:35:46 +0800

    initial commit

---
+++ src/Select.tsx
@@ -0,0 +1,143 @@
+import styles from "./select.module.css";
+import React, { useRef, useState, useEffect } from "react";
+
+export type SelectOption = {
+  label: string;
+  value: string | number;
+};
+
+type MultipleSelectProps = {
+  multiple: true;
+  onChange: (value: SelectOption[]) => void;
+  value: SelectOption[];
+};
+type SingleSelectProps = {
+  multiple?: false;
+  onChange: (value: SelectOption | undefined) => void;
+  value?: SelectOption;
+};
+
+type SelectProps = {
+  options: SelectOption[];
+} & (SingleSelectProps | MultipleSelectProps);
+
+export function Select({ multiple, value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  function clearOptions() {
+    multiple ? onChange([]) : onChange(undefined);
+  }
+  function selectOption(option: SelectOption) {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) onChange(option);
+    }
+  }
+  function isOptionSelected(option: SelectOption) {
+    return multiple ? value.includes(option) : option === value;
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      setHighlightedIndex(0);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target != containerRef.current) return;
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen((prev) => !prev);
+          if (isOpen) selectOption(options[highlightedIndex]);
+          break;
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue);
+          }
+          break;
+        }
+        case "Escape":
+          setIsOpen(false);
+          break;
+      }
+    };
+    containerRef.current?.addEventListener("keydown", handler);
+    return () => {
+      containerRef.current?.removeEventListener("keydown", handler);
+    };
+  }, [isOpen, highlightedIndex, options]);
+  return (
+    <div
+      ref={containerRef}
+      tabIndex={0}
+      className={styles.container}
+      onClick={() => {
+        setIsOpen(!isOpen);
+      }}
+      onBlur={() => {
+        setIsOpen(false);
+      }}
+    >
+      <span className={styles.value}>
+        {multiple
+          ? value.map((v) => (
+              <button
+                key={v.value}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(v);
+                }}
+                className={styles["option-badge"]}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </span>
+      <button
+        className={styles["clear-btn"]}
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+      >
+        &times;
+      </button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option, idx) => (
+          <li
+            key={option.value}
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            onMouseEnter={() => setHighlightedIndex(idx)}
+            className={`${styles.option} ${
+              isOptionSelected(option) ? styles.selected : ""
+            } ${idx === highlightedIndex ? styles.highlighted : ""}`}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
